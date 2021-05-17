8e5b01e74e7ca59a7f294378dbc7a44b07a83ee9
gunjan-meethook <68480612+gunjan-meethook@users.noreply.github.com>
2021-05-17 12:38:49 -0500

    Add project files.

---
+++ DeliveryTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryTracker.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<DeliveryTracker.Models.OrderFullfillment> OrderFullfillment { get; set; }
+    }
+}
